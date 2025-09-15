529197a9ec6d3fa06d32e66bb6326d5e6faa8d0a
Carmen M. Camino <UO299841@uniovi.es>
2025-09-15 22:35:59 +0200

    FIxing errors

--- main.py
+++ main.py
@@ -5,15 +5,7 @@ from dotenv import load_dotenv #allows us to load environment variable files
 import os
 
 """
-       _____  .___ __          .__    _________                    .__                 __                 
-   *                       (                                                        
- (  `           )       (  )\ )                    (                  )             
- )\))(   (   ( /(    (  )\(()/(   (      )         )\    )         ( /(    (   (    
-((_)()\  )\  )\())  ))\((_)/(_)) ))\    (      (  ((_)( /(   (     )\())  ))\  )(   
-(_()((_)((_)((_)\  /((_)_ (_))  /((_)   )\  '  )\  _  )(_))  )\ ) ((_)\  /((_)(()\  
-|  \/  | (_)| |(_)(_)) | |/ __|(_))(  _((_))  ((_)| |((_)_  _(_/( | |(_)(_))   ((_) 
-| |\/| | | || / / / -_)| |\__ \| || || '  \()/ _| | |/ _` || ' \))| / / / -_) | '_| 
-|_|  |_| |_||_\_\ \___||_||___/ \_,_||_|_|_| \__| |_|\__,_||_||_| |_\_\ \___| |_|   
+      mikelsumclanker   
       
     Developers:                                                                              
         Mendez Camino, Carmen
@@ -22,29 +14,15 @@ import os
     References: https://discordpy.readthedocs.io/en/stable
         -> Modern Pythonic API using async/await syntax
         
-    Prerequisites:
-        -> Python 3.8 or higher
-        
-    Before We Start Coding [Applied for Windows 10/11 users]:
-        Get the library directly from PyPI: py -3 -m pip install -U discord.py
-        To get voice support, you should use discord.py[voice] instead of discord.py, e.g.: python3 -m pip install -U discord.py[voice]
-        Install python-dotenv: pip install python-dotenv
-
-        VIRTUAL ENVIROMENTS!!!
-            $ cd your-bot-source
-            $ python3 -m venv bot-env
-            $ bot-env\Scripts\activate.bat
-            $ pip install -U discord.py
-
-    Basic Concepts: discord.py follows an event-oriented programming paradigm
+    
 
 """ 
 load_dotenv() # this will load the current environment variable file
 token = os.getenv('DISCORD_TOKEN')
 
 # Set up logging
-handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w') # mode w -> write mode 
-logging.basicConfig(level=logging.INFO, handlers=[handler]) # mode w -> write mode
+handler = logging.FileHandler(filename='logs/discord.log', encoding='utf-8', mode='w') # mode w -> write mode 
+logging.basicConfig(level=logging.DEBUG, handlers=[handler]) # mode w -> write mode
 
 # Intents specify which events the bot will receive
 intents = discord.Intents.default()
